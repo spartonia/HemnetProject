7d98342904d9ed58dc9ab4598cec5c832e9c87d0
S.Partonia <sd_prtn@yahoo.com>
2020-09-04 22:16:28 +0200

    Dag for historicSoldSpider

--- dags/hemnet_historic_sold_workflow.py
+++ dags/hemnet_historic_sold_workflow.py
@@ -9,7 +9,7 @@ HEMNET_SPIDER_DOCKER_IMAGE = 'hemnet-spiders:latest'
 default_args = {
         'owner'                 : 'airflow',
         'depend_on_past'        : False,
-        'start_date'            : datetime(2020, 8, 29),
+        'start_date'            : datetime(2020, 9, 4),
         'email_on_failure'      : False,
         'email_on_retry'        : False,
         'retries'               : 1,
@@ -19,24 +19,43 @@ default_args = {
 dag = DAG(
     'Hemnet_historic_sold_workflow',
     default_args=default_args,
-    description='Pipeline for scraping daily "forsale" data from hemnet and \
-        ingesting to deltalake on S3',
-    schedule_interval='1 8 2 9 *' # At 08:01 on day-of-month 2 in September.
+    description='Pipeline for scraping historic sold data from hemnet and \
+        publishing to kafka',
+    schedule_interval='27 0-18 * * *' # At minute 27 past every hour from 0 through 18.
 )
 
-historic_sold_url_cmd = """
-    historicSoldURLCollector \
+# historic_sold_url_cmd = """
+#     historicSoldURLCollector \
+#     -s REDIS_HOST={{ var.value.REDIS_HOST }} \
+#     -a MAX_LOC_PER_RUN=50
+# """
+
+# scrape_sold_urls_to_redis = DockerOperator(
+#     task_id='hemnet_historic_sold_urls_spider',
+#     image=HEMNET_SPIDER_DOCKER_IMAGE,
+#     command=historic_sold_url_cmd,
+#     docker_url='unix://var/run/docker.sock',
+#     network_mode='host',
+#     dag=dag
+# )
+
+# scrape_sold_urls_to_redis
+
+historic_sold_downloader = """
+    historicSoldSpider \
+    -a MAX_ITEMS_PER_RUN=1200 \
     -s REDIS_HOST={{ var.value.REDIS_HOST }} \
-    -a MAX_LOC_PER_RUN=50
+    -s KAFKA_PRODUCER_TOPIC={{ var.value.KAFKA_TOPIC_SOLD }} \
+    -s KAFKA_PRODUCER_BROKERS={{ var.value.KAFKA_BROKERS }}
 """
 
-scrape_sold_urls_to_redis = DockerOperator(
-    task_id='hemnet_historic_sold_urls_spider',
+scrape_pages_to_kafka = DockerOperator(
+    task_id='hemnet_historic_sold_downloader_spider',
     image=HEMNET_SPIDER_DOCKER_IMAGE,
-    command=historic_sold_url_cmd,
+    command=historic_sold_downloader,
     docker_url='unix://var/run/docker.sock',
     network_mode='host',
     dag=dag
 )
 
-scrape_sold_urls_to_redis
+scrape_pages_to_kafka
