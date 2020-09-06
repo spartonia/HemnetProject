17c8d8aa1371e8ad4ceb78106e432efbd01af5ba
S.Partonia <sd_prtn@yahoo.com>
2020-09-07 00:56:42 +0200

    Dag for soldRefinedSilver

--- dags/hemnet_daily_sold_workflow.py
+++ dags/hemnet_daily_sold_workflow.py
@@ -68,4 +68,29 @@ kafka_to_bronze = SSHOperator(
     dag=dag)
 
 
-scrape_pages_to_kafka >> kafka_to_bronze
+spark_submit_cmd_sold_silver = """
+cd {{ var.value.ETL_HOME }}
+{{ var.value.SPARK_HOME }}/spark-submit \
+    --packages io.delta:delta-core_2.12:0.7.0,org.apache.hadoop:hadoop-aws:2.7.7,org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0  \
+    --conf spark.delta.logStore.class=org.apache.spark.sql.delta.storage.S3SingleDriverLogStore  \
+    --conf spark.hadoop.fs.s3a.endpoint={{ var.value.S3_ENDPOINT }}  \
+    --conf spark.driver.extraJavaOptions=-Dcom.amazonaws.services.s3.enableV4=true  \
+    --conf spark.executor.extraJavaOptions=-Dcom.amazonaws.services.s3.enableV4=true  \
+    --conf spark.hadoop.fs.s3a.access.key={{ var.value.AWS_S3_ACCESS }}  \
+    --conf spark.hadoop.fs.s3a.secret.key={{ var.value.AWS_S3_SECRET }} \
+    --py-files=dist/jobs.zip,dist/libs.zip dist/main.py  \
+    --job soldRefinedSilver  \
+    --job-args  \
+        S3_SOURCE={{ var.value.S3_SINK_SOLD_BRONZE }}  \
+        S3_SINK={{ var.value.S3_SINK_SOLD_SILVER }}  \
+        FOR_DATE={{ tomorrow_ds }}
+"""
+
+bronze_to_silver = SSHOperator(
+    ssh_conn_id='ssh_alp-XPS-13-9380',
+    task_id='soldToSilverTask',
+    command=spark_submit_cmd_sold_silver,
+    dag=dag)
+
+
+scrape_pages_to_kafka >> kafka_to_bronze >> bronze_to_silver
